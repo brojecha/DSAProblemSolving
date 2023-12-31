42b4afcc07c41238ba3f1122ddbe9af5f5a528b0
Bikash Mohapatra <mbikash.ph@gmail.com>
2023-12-31 21:46:23 +0530

    New Code added.

--- src/com/technostack/gfg/SearchElementInArray.java
+++ src/com/technostack/gfg/SearchElementInArray.java
@@ -1,2 +1,27 @@
-package com.technostack.gfg;public class SearchElementInArray {
+package com.technostack.gfg;
+
+import java.util.Scanner;
+
+public class SearchElementInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] myArray = new int[n];
+        for(int i = 0;i<=myArray.length-1;i++){
+            myArray[i] = scanner.nextInt();
+        }
+        int x = scanner.nextInt();
+        int value = searchElemntInArray(n, myArray, x);
+        System.out.println("Index returned Value"+" "+value);
+    }
+
+    static int searchElemntInArray(int n,int[] myArray,int x){
+        for(int i = 0;i <= myArray.length-1;i++){
+            if(myArray[i]==x){
+              return i;
+            }
+        }
+        return -1;
+    }
 }
