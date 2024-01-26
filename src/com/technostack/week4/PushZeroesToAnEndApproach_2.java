2241a1a88445be42773fd02d2d2a327446f525a4
Bikash Mohapatra <mbikash.ph@gmail.com>
2024-01-26 22:58:55 +0530

    New Code addeed.

---
+++ src/com/technostack/week4/PushZeroesToAnEndApproach_2.java
@@ -0,0 +1,32 @@
+package com.technostack.week4;
+
+import java.util.Arrays;
+
+public class PushZeroesToAnEndApproach_2 {
+    public static void main(String[] args) {
+        int[] array = {4,5,0,1,0};
+        pushZeroesToAnEndImplementation_Approach2(array);
+    }
+
+    private static void pushZeroesToAnEndImplementation_Approach2(int[] array) {
+       //In this approach we are going to changes everything in the same array
+        // Here the return type is void
+
+        int count = 0;
+        for(int value : array){
+            if(value != 0){
+                array[count] = value;
+                count++;
+            }
+        }
+
+        while(count < array.length){
+            array[count] = 0;
+            count++;
+        }
+
+        for(int myVal : array){
+            System.out.print(" "+myVal);
+        }
+    }
+}
